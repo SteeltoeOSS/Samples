cc92d384a7cbf6f44877230657bf95181202cb64
Dave Tillman <dtillman@vmware.com>
2016-09-14 08:17:40 -0600

    Fix postgres efcore sample seed problem

--- Connectors/src/PostgreEFCore/Models/SampleData.cs
+++ Connectors/src/PostgreEFCore/Models/SampleData.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore.Storage;
 using Microsoft.Extensions.DependencyInjection;
 using System;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace PostgreEFCore
@@ -18,12 +19,10 @@ namespace PostgreEFCore
             using (var serviceScope = serviceProvider.GetRequiredService<IServiceScopeFactory>().CreateScope())
             {
                 var db = serviceScope.ServiceProvider.GetService<TestContext>();
-         
-                await db.Database.MigrateAsync();
+                await db.Database.EnsureCreatedAsync();
+               
             }
-
             await InitializeContext(serviceProvider);
-
         }
 
         private static async Task InitializeContext(IServiceProvider serviceProvider)
@@ -31,19 +30,25 @@ namespace PostgreEFCore
             using (var serviceScope = serviceProvider.GetRequiredService<IServiceScopeFactory>().CreateScope())
             {
                 var db = serviceScope.ServiceProvider.GetService<TestContext>();
+                if (DataExists<TestData>(db))
+                    return;
+
                 AddData<TestData>(db, new TestData() { Id = 1, Data = "Test Data 1 - TestContext " });
                 AddData<TestData>(db, new TestData() { Id = 2, Data = "Test Data 2 - TestContext " });
                 await db.SaveChangesAsync();
             }
         }
 
+        private static bool DataExists<TData>(DbContext db) where TData: class
+        {
+            var existingData = db.Set<TData>().ToList();
+            if (existingData.Count > 0)
+                return true;
+            return false;
+        }
+
         private static void AddData<TData>(DbContext db, object item) where TData: class
         {
-            if (db.Entry(item) != null)
-            {
-                return;
-            }
-
             db.Entry(item).State = EntityState.Added;
         }
     }
