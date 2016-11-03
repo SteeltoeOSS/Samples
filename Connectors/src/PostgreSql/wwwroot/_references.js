dbc512c6c71b1ac9a2eb9bd3875949b4f27d4bb8
Dave Tillman <dtillman@vmware.com>
2016-11-03 14:45:53 -0600

    Polish Postgres sample

--- Connectors/src/PostgreSql/wwwroot/_references.js
+++ Connectors/src/PostgreSql/wwwroot/_references.js
@@ -1,4 +1,7 @@
 ﻿/// <autosync enabled="true" />
+/// <reference path="../publish/wwwroot/_references.js" />
+/// <reference path="../publish/wwwroot/js/site.js" />
+/// <reference path="../publish/wwwroot/lib/jquery-validation/dist/jquery.validate.min.js" />
 /// <reference path="js/site.js" />
 /// <reference path="lib/bootstrap/dist/js/bootstrap.js" />
 /// <reference path="lib/jquery/dist/jquery.js" />
