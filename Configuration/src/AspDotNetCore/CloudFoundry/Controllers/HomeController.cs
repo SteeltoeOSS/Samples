60f3a4b53f98025dbf48b9ddb0e76b3128090fb1
Dave Tillman <dtillman@pivotal.io>
2017-03-30 14:51:35 -0600

    Add sample for CloudFoundry provider

---
+++ Configuration/src/AspDotNetCore/CloudFoundry/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Steeltoe.Extensions.Configuration.CloudFoundry;
+using 
+namespace CloudFoundry.Controllers
+{
+    public class HomeController : Controller
+    {
+        private CloudFoundryServicesOptions CloudFoundryServices { get; set; }
+        private CloudFoundryApplicationOptions CloudFoundryApplication { get; set; }
+
+        public HomeController(
+            IOptions<CloudFoundryApplicationOptions> appOptions, 
+            IOptions<CloudFoundryServicesOptions> servOptions)
+        {
+            CloudFoundryServices = servOptions.Value;
+            CloudFoundryApplication = appOptions.Value;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+
+        public IActionResult CloudFoundry()
+        {
+            return View(new CloudFoundryViewModel(
+                CloudFoundryApplication == null ? new CloudFoundryApplicationOptions() : CloudFoundryApplication,
+                CloudFoundryServices == null ? new CloudFoundryServicesOptions() : CloudFoundryServices));
+        }
+    }
+}
---
+++ Configuration/src/AspDotNetCore/CloudFoundry/ViewModels/Home/CloudFoundryViewModel.cs
@@ -0,0 +1,16 @@
+﻿using Steeltoe.Extensions.Configuration.CloudFoundry;
+
+
+namespace CloudFoundry.ViewModels.Home
+{
+    public class CloudFoundryViewModel
+    {
+        public CloudFoundryViewModel(CloudFoundryApplicationOptions appOptions, CloudFoundryServicesOptions servOptions)
+        {
+            CloudFoundryServices = servOptions;
+            CloudFoundryApplication = appOptions;
+        }
+        public CloudFoundryServicesOptions CloudFoundryServices { get;}
+        public CloudFoundryApplicationOptions CloudFoundryApplication { get;}
+    }
+}
