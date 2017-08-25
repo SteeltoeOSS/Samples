6837dea4a40a55444c942923f854f94c5b50e8f2
Dave Tillman <dtillman@pivotal.io>
2017-08-25 13:32:31 -0600

    Add some Logging to Asp.NET 4 Discovery Sample

--- Discovery/src/AspDotNet4/Fortune-Teller-UI4/Controllers/HomeController.cs
+++ Discovery/src/AspDotNet4/Fortune-Teller-UI4/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using FortuneTellerUI4.Services;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,19 +12,23 @@ namespace FortuneTellerUI4.Controllers
     public class HomeController : Controller
     {
         IFortuneService _fortunes;
+        ILogger<HomeController> _logger;
 
-        public HomeController(IFortuneService fortunes)
+        public HomeController(IFortuneService fortunes, ILoggerFactory logFactory = null)
         {
             _fortunes = fortunes;
+            _logger = logFactory?.CreateLogger<HomeController>();
         }
 
         public ActionResult Index()
         {
+            _logger?.LogInformation("Index");
             return View();
         }
 
         public async Task<string> Random()
         {
+            _logger?.LogInformation("Random");
             return  await _fortunes.RandomFortuneAsync();
         }
     }
