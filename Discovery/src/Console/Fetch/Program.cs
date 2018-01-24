cba2f2f76e340b32226b7bb4c85833c432223d4c
Dave Tillman <dtillman@pivotal.io>
2018-01-24 09:41:53 -0700

    Add Console sample application for Discovery

---
+++ Discovery/src/Console/Fetch/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Steeltoe.Discovery.Eureka;
+
+namespace Fetch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Build application configuration
+            ConfigurationBuilder builder = new ConfigurationBuilder();
+            builder.SetBasePath(Environment.CurrentDirectory);
+            builder.AddJsonFile("appsettings.json");
+            var configuration = builder.Build();
+
+            // Setup logging
+            var factory = new LoggerFactory();
+            factory.AddConsole(configuration.GetSection("Logging"));
+
+            // Build Eureka clients config from configuration
+            var clientConfig = new EurekaClientConfig();
+            ConfigurationBinder.Bind(configuration.GetSection("eureka:client"), clientConfig);
+
+            // Create the Eureka client, start fetching registry in background thread
+            var client = new DiscoveryClient(clientConfig, null, factory);
+
+            do
+            {
+                // Get what applications have been fetched
+                var apps = client.Applications;
+
+                // Try to find app with name "MyApp", it is registered in the Register console application
+                var app = apps.GetRegisteredApplication("MyApp");
+                if (app != null)
+                {
+                    // Print the instance info, and then exit loop
+                    Console.WriteLine("Successfully fetched application: {0} ", app);
+                    break;
+                }
+
+            } while (true);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Discovery/src/Console/Register/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Steeltoe.Discovery.Eureka;
+
+namespace Register
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Build application configuration
+            ConfigurationBuilder builder = new ConfigurationBuilder();
+            builder.SetBasePath(Environment.CurrentDirectory);
+            builder.AddJsonFile("appsettings.json");
+            var configuration = builder.Build();
+
+
+            // Setup logging
+            var factory = new LoggerFactory();
+            factory.AddConsole(configuration.GetSection("Logging"));
+
+
+            // Build Eureka clients config from configuration
+            var clientConfig = new EurekaClientConfig();
+            ConfigurationBinder.Bind(configuration.GetSection("eureka:client"), clientConfig);
+
+            // Build Eureka instance info config from configuration
+            var instConfig = new EurekaInstanceConfig();
+            ConfigurationBinder.Bind(configuration.GetSection("eureka:instance"), instConfig);
+
+            // Initialize ApplicationManager with instance configuration
+            ApplicationInfoManager.Instance.Initialize(instConfig, factory);
+
+            // Create the Eureka client, the Application is registered and renewed with registry
+            var client = new DiscoveryClient(clientConfig, null, factory);
+
+            // Hang and keep renewing the registration
+            Console.ReadLine();
+        }
+    }
+}
