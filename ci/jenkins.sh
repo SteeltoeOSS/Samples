f33b1d1fc569f2aef1a29a267bb7916debbce603
Chris Cheetham <chris@cheetham.com>
2018-03-28 13:51:53 -0400

    Stub for running on Jenkins

---
+++ ci/jenkins.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+basedir=`dirname $0`/..
+userini=$basedir/user.ini
+
+cat > $userini <<EOF
+cf_apiurl = api.run.pcfbeta.io
+cf_org = STEELTOE
+cf_username = `echo $STEELTOE_PCF_CREDENTIALS | cut -d: -f1`
+cf_password = `echo $STEELTOE_PCF_CREDENTIALS | cut -d: -f2`
+EOF
