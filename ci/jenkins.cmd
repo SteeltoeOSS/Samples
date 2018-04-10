cdfa1e8a0bd8fa63812c7f214609aa71c6233abf
Chris Cheetham <chris@cheetham.com>
2018-04-10 12:29:54 -0400

    Use unique test out for each sample

--- ci/jenkins.cmd
+++ ci/jenkins.cmd
@@ -12,13 +12,14 @@ SET DOTNET_HOME=C:\Program Files\dotnet
 SET JAVA_HOME=C:\opt\oracle-jdk-8
 SET GIT_HOME=C:\Program Files\Git
 SET MAVEN_HOME=C:\opt\apache-maven-3.3.9
+SET TEST_OUT=C:\st\%~nx1
 
 ECHO [behave.userdata] > %USERINI%
 ECHO cf_apiurl = api.run.pcfbeta.io >> %USERINI%
 ECHO cf_org = STEELTOE >> %USERINI%
 ECHO cf_username = %CF_USER% >> %USERINI%
 ECHO cf_password = %CF_PASS% >> %USERINI%
-ECHO output = C:\steeltoe-test >> %USERINI%
+ECHO output = %TEST_OUT% >> %USERINI%
 
 SET PATH=%PYTHON_HOME%\Scripts;%DOTNET_HOME%;%JAVA_HOME%\bin;%GIT_HOME%\bin;%MAVEN_HOME%\bin;%PATH%
 
