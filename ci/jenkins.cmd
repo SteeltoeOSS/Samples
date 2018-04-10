98d651b625623531688795ae85da930801979302
Chris Cheetham <chris@cheetham.com>
2018-04-10 11:53:43 -0400

    Update path to DotNet

--- ci/jenkins.cmd
+++ ci/jenkins.cmd
@@ -8,6 +8,7 @@ FOR /F "tokens=1,2 delims=:" %%a in ("%STEELTOE_PCF_CREDENTIALS%") do (
 )
 
 SET PYTHON_HOME=C:\Python36
+SET DOTNET_HOME=C:\Program Files\dotnet
 SET MAVEN_HOME=C:\opt\apache\apache-maven-3.3.9
 
 ECHO [behave.userdata] > %USERINI%
@@ -16,7 +17,7 @@ ECHO cf_org = STEELTOE >> %USERINI%
 ECHO cf_username = %CF_USER% >> %USERINI%
 ECHO cf_password = %CF_PASS% >> %USERINI%
 
-SET PATH=%PYTHON_HOME%\Scripts;%MAVEN_HOME%\bin;%PATH%
+SET PATH=%PYTHON_HOME%\Scripts;%DOTNET_HOME%;%MAVEN_HOME%\bin;%PATH%
 
 CALL %BASEDIR%\test-setup
 CALL %BASEDIR%\test-run %*
