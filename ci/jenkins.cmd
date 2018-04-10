e3b69021ee8ebbb0199edf1b92c449b4db73d968
Chris Cheetham <chris@cheetham.com>
2018-04-10 10:57:51 -0400

    Jenkins launcher for Windows

---
+++ ci/jenkins.cmd
@@ -0,0 +1,20 @@
+@ECHO OFF
+
+SET BASEDIR=%~dp0..
+SET USERINI=%BASEDIR%\user.ini
+FOR /F "tokens=1,2 delims=:" %%a in ("%STEELTOE_PCF_CREDENTIALS%") do (
+   set CF_USER=%%a
+   set CF_PASS=%%b
+)
+SET MAVEN_HOME=C:\opt\apache\apache-maven-3.5.3
+
+ECHO [behave.userdata] > %USERINI%
+ECHO cf_apiurl = api.run.pcfbeta.io >> %USERINI%
+ECHO cf_org = STEELTOE >> %USERINI%
+ECHO cf_username = %CF_USER% >> %USERINI%
+ECHO cf_password = %CF_PASS% >> %USERINI%
+
+SET PATH=%MAVEN_HOME%\bin;%PATH%
+
+CALL %BASEDIR%\test-setup
+CALL %BASEDIR%\test-run %*
