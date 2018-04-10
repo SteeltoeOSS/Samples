b9640e6de8a5b3095f29e612ac9076cd91d373d3
Chris Cheetham <chris@cheetham.com>
2018-04-10 14:57:02 -0400

    Update path to CloundFoundry CLI

--- ci/jenkins.cmd
+++ ci/jenkins.cmd
@@ -12,6 +12,7 @@ SET DOTNET_HOME=C:\Program Files\dotnet
 SET JAVA_HOME=C:\opt\oracle-jdk-8
 SET GIT_HOME=C:\Program Files\Git
 SET MAVEN_HOME=C:\opt\apache-maven-3.3.9
+SET CF_HOME=C:\ProgramData\chocolatey\bin
 SET TEST_OUT=C:\st\%~nx1
 
 ECHO [behave.userdata] > %USERINI%
@@ -21,7 +22,7 @@ ECHO cf_username = %CF_USER% >> %USERINI%
 ECHO cf_password = %CF_PASS% >> %USERINI%
 ECHO output = %TEST_OUT% >> %USERINI%
 
-SET PATH=%PYTHON_HOME%\Scripts;%DOTNET_HOME%;%JAVA_HOME%\bin;%GIT_HOME%\bin;%MAVEN_HOME%\bin;%PATH%
+SET PATH=%PYTHON_HOME%\Scripts;%DOTNET_HOME%;%JAVA_HOME%\bin;%GIT_HOME%\bin;%MAVEN_HOME%\bin;%PATH%;%CF_HOME%
 
 RMDIR /S /Q %TEST_OUT%
 CALL %BASEDIR%\test-setup
