37d16272fd55a2d62fff935c9370661ead03efb3
Chris Cheetham <chris@cheetham.com>
2018-04-10 11:38:21 -0400

    Update paths to Python and Maven

--- ci/jenkins.cmd
+++ ci/jenkins.cmd
@@ -6,7 +6,9 @@ FOR /F "tokens=1,2 delims=:" %%a in ("%STEELTOE_PCF_CREDENTIALS%") do (
    set CF_USER=%%a
    set CF_PASS=%%b
 )
-SET MAVEN_HOME=C:\opt\apache\apache-maven-3.5.3
+
+SET PYTHON_HOME=C:\Python36
+SET MAVEN_HOME=C:\opt\apache\apache-maven-3.3.9
 
 ECHO [behave.userdata] > %USERINI%
 ECHO cf_apiurl = api.run.pcfbeta.io >> %USERINI%
@@ -14,7 +16,7 @@ ECHO cf_org = STEELTOE >> %USERINI%
 ECHO cf_username = %CF_USER% >> %USERINI%
 ECHO cf_password = %CF_PASS% >> %USERINI%
 
-SET PATH=%MAVEN_HOME%\bin;%PATH%
+SET PATH=%PYTHON_HOME%\Scripts;%MAVEN_HOME%\bin;%PATH%
 
 CALL %BASEDIR%\test-setup
 CALL %BASEDIR%\test-run %*
