fc9e90907700bb931a9423e4d36e3be43438298b
Chris Cheetham <chris@cheetham.com>
2018-06-23 12:57:54 -0400

    Move template project to top level dir

--- TemplatePack/Properties/AssemblyInfo.cs
+++ TemplatePack/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ConfigurationTemplatePackProject")]
+[assembly: AssemblyTitle("TemplatePackProject")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ConfigurationTemplatePackProject")]
+[assembly: AssemblyProduct("TemplatePackProject")]
 [assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
