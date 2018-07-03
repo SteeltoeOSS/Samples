4232448225021039473fd4a1f3205c9d21d77c11
Tim Hess <thess@pivotal.io>
2018-07-03 11:54:53 -0500

    Improved HttpModule registration

--- Management/src/AspDotNet4/ManagementSysWeb/Global.asax.cs
+++ Management/src/AspDotNet4/ManagementSysWeb/Global.asax.cs
@@ -1,11 +1,11 @@
 ﻿using Autofac;
-using Autofac.Extras.CommonServiceLocator;
-using CommonServiceLocator;
 using ManagementSysWeb.App_Start;
 using Steeltoe.Common.Configuration.Autofac;
 using Steeltoe.Extensions.Configuration.CloudFoundry;
 using Steeltoe.Management.EndpointAutofac;
+using Steeltoe.Management.EndpointSysWeb;
 using System;
+using System.Collections.Generic;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
@@ -14,10 +14,31 @@ namespace ManagementSysWeb
 {
     public class Global : HttpApplication
     {
+        public static IContainer Container;
+
         void Application_Start(object sender, EventArgs e)
         {
+            // Code that runs on application startup
+            ApplicationConfig.Register("development");
+            ApplicationConfig.ConfigureLogging();
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+
+            var builder = new ContainerBuilder();
+            builder.RegisterCloudFoundryOptions(ApplicationConfig.Configuration);
+            builder.RegisterConfiguration(ApplicationConfig.Configuration);
+            builder.RegisterCloudFoundryModules(ApplicationConfig.Configuration);
+
+            Container = builder.Build();
+        }
+
+        public override void Init()
+        {
+            // modules can be activated before or after base.Init(), 
+            // just not during Application_Start or you'll get null references
+            var modules = Container.Resolve<IEnumerable<IHttpModule>>();
+            CloudFoundryModules.ActivateCloudFoundryModules(this, modules);
+            base.Init();
         }
     }
 }
