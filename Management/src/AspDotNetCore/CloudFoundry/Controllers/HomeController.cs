34b98f0d4b770f9bbe219089888cf27f648299f3
Dave Tillman <dtillman@pivotal.io>
2018-07-31 14:26:21 -0600

    Add controller random sleep in Management sample

--- Management/src/AspDotNetCore/CloudFoundry/Controllers/HomeController.cs
+++ Management/src/AspDotNetCore/CloudFoundry/Controllers/HomeController.cs
@@ -8,21 +8,25 @@ namespace CloudFoundry.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger _logger;
+        private readonly Random _random;
 
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
+            _random = new Random();
         }
 
         public IActionResult Index()
         {
-            Thread.Sleep(1000);
+            Thread.Sleep(_random.Next(500, 2000));
+
             _logger.LogCritical("Test Critical message");
             _logger.LogError("Test Error message");
             _logger.LogWarning("Test Warning message");
             _logger.LogInformation("Test Informational message");
             _logger.LogDebug("Test Debug message");
             _logger.LogTrace("Test Trace message");
+
             return View();
         }
 
