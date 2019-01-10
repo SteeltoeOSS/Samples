5bdb6ff41b8fd85c69760e190e43a98bcaaeca93
Dave Tillman <dtillman@pivotal.io>
2019-01-10 10:57:24 -0700

    Add GetLoggerFactory sample code

--- Configuration/src/AspDotNetCore/SimpleCloudFoundry/Program.cs
+++ Configuration/src/AspDotNetCore/SimpleCloudFoundry/Program.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Console;
+using Microsoft.Extensions.Logging.Debug;
 using Pivotal.Extensions.Configuration.ConfigServer;
 
 namespace SimpleCloudFoundry
@@ -15,8 +18,20 @@ namespace SimpleCloudFoundry
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                     .UseCloudFoundryHosting()
-                    .AddConfigServer(new LoggerFactory().AddConsole(LogLevel.Trace))
+                    .AddConfigServer(GetLoggerFactory())
                     .UseStartup<Startup>()
                     .Build();
+
+        public static ILoggerFactory GetLoggerFactory()
+        {
+            IServiceCollection serviceCollection = new ServiceCollection();
+            serviceCollection.AddLogging(builder => builder.SetMinimumLevel(LogLevel.Trace));
+            serviceCollection.AddLogging(builder => builder.AddConsole((opts) =>
+            {
+                opts.DisableColors = true;
+            }));
+            serviceCollection.AddLogging(builder => builder.AddDebug());
+            return serviceCollection.BuildServiceProvider().GetService<ILoggerFactory>();
+        }
     }
 }
