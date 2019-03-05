2c99c71b12befe44e974220e14ac8462e1de6807
Dave Tillman <dtillman@pivotal.io>
2019-03-05 11:23:49 -0700

    Add a Placeholder sample

---
+++ Configuration/src/AspDotNetCore/Placeholder/SampleOptions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Placeholder
+{
+    public class SampleOptions
+    {
+          public string ResolvedPlaceholderFromEnvVariables { get; set; }
+          public string UnresolvedPlaceholder { get; set; }
+          public string ResolvedPlaceholderFromJson { get; set; }
+    }
+}
