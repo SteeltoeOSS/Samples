fd29e8a0d42353d720ace9d06a8bb0b20fcb746f
Tim Hess <tim.hess@gmail.com>
2019-03-14 12:01:04 -0500

    Add SMB samples [#164639342]

---
+++ FileShares/src/AspNetCore/SMBFileShares/Program.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Steeltoe.Extensions.Configuration.CloudFoundry;
+using System;
+
+namespace SMBFileShares
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .AddCloudFoundry()
+                .UseCloudFoundryHosting()
+                .UseStartup<Startup>();
+    }
+}
