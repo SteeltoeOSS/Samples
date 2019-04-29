1afca39d9d7f9ac80bfddd76be608a9ad5cae729
Tim Hess <tim.hess@gmail.com>
2019-04-29 10:36:24 -0500

    Add a VB SSO front end

---
+++ Security/src/AspDotNet4/CloudFoundrySingleSignon-VB/AuthorizationManager.vb
@@ -0,0 +1,22 @@
+﻿Imports System.Security.Claims
+
+Namespace CloudFoundrySingleSignon
+    Public Class AuthorizationManager
+        Inherits ClaimsAuthorizationManager
+
+        Public Overrides Function CheckAccess(ByVal context As AuthorizationContext) As Boolean
+            Dim requiredClaims = context.Action.[Select](Function(v) v.Value)
+            Dim possessedClaims = context.Principal.Claims.[Select](Function(v) v.Value)
+            Dim claim = (CType(context.Principal.Identity, ClaimsIdentity)).FindFirst("testgroup")
+
+            For Each c In requiredClaims
+
+                If possessedClaims.Contains(c) Then
+                    Return True
+                End If
+            Next
+
+            Return False
+        End Function
+    End Class
+End Namespace
---
+++ Security/src/AspDotNet4/CloudFoundrySingleSignon-VB/Startup.vb
@@ -0,0 +1,14 @@
+﻿Imports Microsoft.Owin
+Imports Owin
+
+<Assembly: OwinStartup(GetType(CloudFoundrySingleSignon.Startup))>
+Namespace CloudFoundrySingleSignon
+
+    Public Class Startup
+        Public Sub Configuration(ByVal app As IAppBuilder)
+            ApplicationConfig.RegisterConfig("development")
+            Security.ConfigureAuth(app)
+        End Sub
+
+    End Class
+End Namespace
