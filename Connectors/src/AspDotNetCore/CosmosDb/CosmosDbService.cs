ac90ea6cb82316b02af2fb2251d6d359f24264b1
Tim Hess <thess@pivotal.io>
2020-04-03 13:20:51 -0400

    add CosmosDb sample

---
+++ Connectors/src/AspDotNetCore/CosmosDb/CosmosDbService.cs
@@ -0,0 +1,37 @@
+﻿using Azure.Cosmos;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CosmosDb
+{
+    public class CosmosDbService : ICosmosDbService
+    {
+        private readonly CosmosContainer _container;
+
+        public CosmosDbService(CosmosClient dbClient, string databaseName, string containerName)
+        {
+            _container = dbClient.GetContainer(databaseName, containerName);
+        }
+
+        public async Task AddTestDataAsync(TestData item)
+        {
+            await _container.CreateItemAsync(item, new PartitionKey(item.Id));
+        }
+
+        public async Task DeleteTestDataAsync(string id)
+        {
+            await _container.DeleteItemAsync<TestData>(id, new PartitionKey(id));
+        }
+
+        public async Task<IEnumerable<TestData>> GetTestDataAsync(string queryString)
+        {
+            var results = new List<TestData>();
+            await foreach (var t in _container.GetItemQueryIterator<TestData>(new QueryDefinition(queryString)))
+            {
+                results.Add(t);
+            }
+
+            return results;
+        }
+    }
+}
---
+++ Connectors/src/AspDotNetCore/CosmosDb/TestData.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace CosmosDb
+{
+    public class TestData
+    {
+        [JsonPropertyName("id")]
+        public string Id { get; set; }
+
+        public string SomeThing { get; set; }
+
+        public string SomeOtherThing { get; set; }
+
+        public override string ToString()
+        {
+            return JsonSerializer.Serialize(this);
+        }
+    }
+}
