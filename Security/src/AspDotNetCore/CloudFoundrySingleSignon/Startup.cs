a1508bbdb4900e53fcc8d2517a01cbb3a0af7fa4
Tim Hess <thess@pivotal.io>
2020-04-07 09:55:53 -0400

    CertificateOptions moved namespaces, adjust job timeouts

--- Security/src/AspDotNetCore/CloudFoundrySingleSignon/Startup.cs
+++ Security/src/AspDotNetCore/CloudFoundrySingleSignon/Startup.cs
@@ -9,6 +9,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Steeltoe.Common.Options;
 using Steeltoe.Common.Security;
 // using Steeltoe.CloudFoundry.Connector.Redis;
 using Steeltoe.Security.Authentication.CloudFoundry;
