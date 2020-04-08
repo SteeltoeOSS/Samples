b35aa270232ba68eef93d28c14749db9e8ff0466
Dave Tillman <dtillman@pivotal.io>
2020-04-08 14:53:21 -0600

    Rabbit mq web sample (#137)
    * Add RabbitMQWeb sample
    
    * Update for M2

---
+++ Messaging/src/AspDotNetCore/RabbitMQWeb/Bar.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RabbitMQWeb
+{
+    public class Bar : Foo
+    {
+        public Bar()
+        {
+        }
+
+        public Bar(string foo)
+            : base(foo)
+        {
+
+        }
+    }
+}
---
+++ Messaging/src/AspDotNetCore/RabbitMQWeb/Foo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RabbitMQWeb
+{
+    public class Foo
+    {
+        public Foo()
+            : base()
+        {
+        }
+
+        public Foo(string foo)
+        {
+            Value = foo;
+        }
+
+
+        public string Value { get; set; }
+
+        public override string ToString()
+        {
+            return GetType().Name + " [foo=" + Value + "]";
+        }
+    }
+}
