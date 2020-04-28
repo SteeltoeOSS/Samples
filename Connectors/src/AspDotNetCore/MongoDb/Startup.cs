216bf6277316a3c3160d84fa4245481ff260498e
Tim Hess <thess@pivotal.io>
2020-04-28 15:44:44 -0400

    Adjust for 2.x differences

--- Connectors/src/AspDotNetCore/MongoDb/Startup.cs
+++ Connectors/src/AspDotNetCore/MongoDb/Startup.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Steeltoe.Connector.MongoDb;
+using Steeltoe.CloudFoundry.Connector.MongoDb;
 
 namespace MongoDb
 {
