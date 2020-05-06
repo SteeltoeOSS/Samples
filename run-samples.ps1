2d1a12ddd8051941aa47a5422f0b8cb9e61f778c
Chris Cheetham <chris@cheetham.com>
2020-05-06 13:27:03 -0400

    Migrate test launcher (Windows) to pipenv

---
+++ run-samples.ps1
@@ -0,0 +1,44 @@
+$ErrorActionPreference = 'stop'
+
+$BaseDir = $PSScriptRoot
+$FrameworkInitFlag = ".framework-initialized"
+$OldPath = $Env:Path
+$Env:Path += ";$Env:AppData\Python\Python38\Scripts"
+
+function Command-Available {
+    param($Command)
+    try {
+        (Get-Command $Command)
+        return $True
+    }
+    catch {
+        return $False
+    }
+}
+
+# ensure pipenv available
+if (!(Command-Available pipenv)) {
+    "installing 'pipenv'"
+    $pip = if (Command-Available pip3) {'pip3'} else {'pip'}
+    & $pip install pipenv --user
+}
+
+try {
+    # set working dir
+    Push-Location $BaseDir
+
+    # initialize framework if needed
+    if (!(Test-Path $FrameworkInitFlag)) {
+        "installing framework"
+        pipenv install --ignore-pipfile
+        New-Item -Name $FrameworkInitFlag -ItemType file | Out-Null
+    }
+
+    # run samples
+    pipenv run behave $Args
+}
+finally {
+    Pop-Location
+    $Env:Path = $OldPath
+}
+
