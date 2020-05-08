0c847522c9cad055e0d5e6ddc067aaf1c47f0f2c
Chris Cheetham <chris@cheetham.com>
2020-05-08 15:34:25 -0400

    Invert behave init logic (non-Windows)

--- behave.sh
+++ behave.sh
@@ -3,7 +3,7 @@
 set -e
 
 basedir=$(dirname $0)
-framework_init_flag=$basedir/.framework-initialized
+reinit_flag=$basedir/reinit
 PATH+=:~/.local/bin
 
 command_available() {
@@ -11,20 +11,28 @@ command_available() {
   command -v $cmd >/dev/null
 }
 
+env_exists() {
+  pipenv --venv >/dev/null 2>&1
+}
+
 # ensure pipenv available
 if ! command_available pipenv >/dev/null; then
   echo "installing 'pipenv'"
-  pip3 install pipenv --user
+  pip3 install --user pipenv
 fi
 
 # set working dir
 cd $basedir
 
-# initialize framework if needed
-if [ ! -f $framework_init_flag ]; then
-  pipenv install --three --ignore-pipfile
-  touch $framework_init_flag
+
+# initialize framework if requested/needed
+if [ -f $reinit_flag ] ; then
+  pipenv --rm || true
+  rm $reinit_flag
+fi
+if ! env_exists; then
+  pipenv --three sync
 fi
 
 # run samples
-exec pipenv run behave $*
+echo exec pipenv run behave $*
