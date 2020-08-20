7fb119577a00897a81a645be215bd8d45262a521
Tim Hess <tim.hess@gmail.com>
2020-08-20 13:26:27 -0400

    Add a Kubernetes configuration sample #162

---
+++ Configuration/src/Kubernetes/Pages/Index.cshtml.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Steeltoe.Common;
+using Steeltoe.Common.Kubernetes;
+
+namespace Kubernetes.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly IApplicationInstanceInfo _applicationInstanceInfo;
+        private readonly IConfiguration _configuration;
+        private readonly ILogger<IndexModel> _logger;
+        public string ConfigMapName;
+        public string UserName;
+        public string K8sNamespace;
+
+        public IndexModel(IApplicationInstanceInfo applicationInstanceInfo, IConfiguration configuration, ILogger<IndexModel> logger)
+        {
+            _applicationInstanceInfo = applicationInstanceInfo;
+            _configuration = configuration;
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            _logger?.LogInformation("OnGetAsync");
+            ConfigMapName = _configuration["configMapName"];
+            UserName = _configuration["username"];
+            if (_applicationInstanceInfo is KubernetesApplicationOptions k8sInfo)
+            {
+                _logger?.LogInformation("Found KubernetesApplicationOptions");
+                K8sNamespace = k8sInfo.NameSpace;
+            }
+        }
+    }
+}
---
+++ Configuration/src/Kubernetes/Program.cs
@@ -0,0 +1,46 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Steeltoe.Common;
+using Steeltoe.Common.Hosting;
+using Steeltoe.Extensions.Configuration.Kubernetes;
+using Steeltoe.Management.Kubernetes;
+
+namespace Kubernetes
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                })
+                .ConfigureAppConfiguration((context, builder) =>
+                {
+                    builder.AddKubernetes(loggerFactory: GetLoggerFactory());
+                    if (Platform.IsKubernetes)
+                    {
+                    }
+                })
+                .AddKubernetesActuators();
+
+        private static ILoggerFactory GetLoggerFactory()
+        {
+            IServiceCollection serviceCollection = new ServiceCollection();
+            serviceCollection.AddLogging(builder => builder.SetMinimumLevel(LogLevel.Trace));
+            serviceCollection.AddLogging(builder => builder.AddConsole((opts) =>
+            {
+                opts.DisableColors = true;
+            }));
+            serviceCollection.AddLogging(builder => builder.AddDebug());
+            return serviceCollection.BuildServiceProvider().GetService<ILoggerFactory>();
+        }
+    }
+}
