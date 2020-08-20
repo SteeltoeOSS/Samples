c86e5fe40a9d48f95b944239e42f800298a779d5
Hananiel Sarella <hananiel@gmail.com>
2020-08-20 16:41:08 -0400

    fix tracing

--- Management/src/Tracing/Fortune-Teller-UI/Services/FortuneService.cs
+++ Management/src/Tracing/Fortune-Teller-UI/Services/FortuneService.cs
@@ -22,28 +22,26 @@ namespace Fortune_Teller_UI.Services
         public async Task<Fortune> RandomFortuneAsync()
         {
             var response = await _httpClient.GetAsync(RANDOM_FORTUNE_URL);
-            var result = Decode(await response.Content.ReadAsStreamAsync());
+            var result =  Decode(await response.Content.ReadAsStringAsync());
             _logger.LogInformation("RandomFortuneAsync: {0}", result.Text);
             return result;
         }
 
-        private Fortune Decode(Stream stream)
+        private Fortune Decode(string json)
         {
             try
-            {
-                var result = JsonSerializer.DeserializeAsync<Fortune>(stream);
-
+            { 
+                return JsonSerializer.Deserialize<Fortune>(json, new JsonSerializerOptions(){PropertyNameCaseInsensitive = true});
             }
             catch (Exception e)
             {
                 _logger?.LogError("Error {0} deserializing", e);
+                return new Fortune()
+                {
+                    Id = 0,
+                    Text = "Have a good day!"
+                };
             }
-
-            return new Fortune()
-            {
-                Id = 0,
-                Text = "Have a good day!"
-            };
         }
     }
 }
