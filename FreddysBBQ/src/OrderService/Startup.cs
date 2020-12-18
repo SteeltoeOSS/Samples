30d2de836b6cd08624ffb110cad0d085fddbf558
Ken Swan <kswan@vmware.com>
2020-12-18 09:04:23 -0600

    unused ref

--- FreddysBBQ/src/OrderService/Startup.cs
+++ FreddysBBQ/src/OrderService/Startup.cs
@@ -5,7 +5,6 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
 using OrderService.Models;
 using Steeltoe.Discovery.Client;
 using Steeltoe.CloudFoundry.Connector.MySql.EFCore;
