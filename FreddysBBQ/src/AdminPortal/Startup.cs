f25ecb3b60500c276986168e86c1afb6c0e44ece
Ken Swan <kswan@vmware.com>
2021-01-04 07:36:06 -0600

    Add Authorization middleware

--- FreddysBBQ/src/AdminPortal/Startup.cs
+++ FreddysBBQ/src/AdminPortal/Startup.cs
@@ -78,6 +78,8 @@ namespace AdminPortal
 
             app.UseAuthentication();
 
+            app.UseAuthorization();
+
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
