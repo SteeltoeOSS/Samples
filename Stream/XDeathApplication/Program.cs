26598dd573abd75dada8c74074a1990ccc83eee3
Hananiel Sarella <hsarella@vmware.com>
2021-05-20 16:38:47 -0400

    Update XDeath-Application sample  (#202)
    * Initial
    
    * make it work
    
    * Update xdeath sample
    
    * Update Basic.csproj
    
    revert accidental commit

---
+++ Stream/XDeathApplication/Program.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Steeltoe.Messaging.Handler.Attributes;
+using Steeltoe.Messaging.RabbitMQ.Exceptions;
+using Steeltoe.Stream.Attributes;
+using Steeltoe.Stream.Messaging;
+using Steeltoe.Stream.StreamHost;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace XDeathApplication
+{
+    [EnableBinding(typeof(ISink))]
+    public class Program
+    {
+        static async Task Main(string[] args)
+        {
+              var host = await StreamHost.CreateDefaultBuilder<Program>(args)
+             .StartAsync();
+
+        }
+
+        [StreamListener(ISink.INPUT)]
+        public void Listen(string input, 
+            [Header(Name ="x-death", Required = false)]
+            IDictionary<string, object> death)
+        {
+            if (death != null && (long)death["count"] == 3L)
+            {
+                // giving up - don't send to DLX
+                throw new ImmediateAcknowledgeException("Failed after 4 attempts");
+            }
+            throw new RabbitRejectAndDontRequeueException("failed");
+        }
+
+    }
+  
+}
