0e77e16c2b7a1f535f714f0956eee711e8a5b071
Tim Hess <thess@vmware.com>
2021-07-13 10:16:52 -0500

    Switch to GenericHost, resolve build errors #197

--- Discovery/src/Console/Fetch/Program.cs
+++ Discovery/src/Console/Fetch/Program.cs
@@ -1,72 +1,24 @@
-﻿using System;
-using System.Linq;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Steeltoe.Connector;
-using Steeltoe.Connector.Services;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Steeltoe.Discovery.Client;
 using Steeltoe.Discovery.Eureka;
 using Steeltoe.Extensions.Configuration.CloudFoundry;
+using System.Threading.Tasks;
 
 namespace Fetch
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            // Build application configuration
-            ConfigurationBuilder builder = new ConfigurationBuilder();
-            builder.SetBasePath(Environment.CurrentDirectory);
-            builder.AddJsonFile("appsettings.json");
-            builder.AddCloudFoundry();
-            var configuration = builder.Build();
-
-            // Setup logging
-            var factory = GetLoggerFactory();
-
-            // Build Eureka clients config from configuration
-            var clientOpts = new EurekaClientOptions();
-            ConfigurationBinder.Bind(configuration.GetSection("eureka:client"), clientOpts);
-
-            // if a Cloud Foundry Service Binding is found, update config
-            var si = configuration.GetServiceInfos<EurekaServiceInfo>();
-            if (si.Any())
-            {
-                EurekaPostConfigurer.UpdateConfiguration(configuration, si.First(), clientOpts);
-            }
-
-            // Create the Eureka client, start fetching registry in background thread
-            var client = new DiscoveryClient(clientOpts, null, factory);
-
-            do
-            {
-                // Get what applications have been fetched
-                var apps = client.Applications;
-
-                // Try to find app with name "MyApp", it is registered in the Register console application
-                var app = apps.GetRegisteredApplication("MyApp");
-                if (app != null)
+            await Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration(config => config.AddCloudFoundry())
+                .ConfigureServices(services =>
                 {
-                    // Print the instance info, and then exit loop
-                    Console.WriteLine("Successfully fetched application: {0} ", app);
-                    break;
-                }
-
-            } while (true);
-
-            Console.ReadLine();
-        }
-
-        public static ILoggerFactory GetLoggerFactory()
-        {
-            IServiceCollection serviceCollection = new ServiceCollection();
-            serviceCollection.AddLogging(builder => builder.SetMinimumLevel(LogLevel.Trace));
-            serviceCollection.AddLogging(builder => builder.AddConsole((opts) =>
-            {
-                opts.DisableColors = true;
-            }));
-            serviceCollection.AddLogging(builder => builder.AddConsole());
-            return serviceCollection.BuildServiceProvider().GetService<ILoggerFactory>();
+                    services.AddServiceDiscovery(client => client.UseEureka());
+                    services.AddHostedService<ConsoleFetchService>();
+                })
+                .RunConsoleAsync();
         }
     }
 }
--- Discovery/src/Console/Register/Program.cs
+++ Discovery/src/Console/Register/Program.cs
@@ -1,69 +1,19 @@
-﻿using System;
-using System.Linq;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Steeltoe.Connector;
-using Steeltoe.Connector.Services;
+﻿using Microsoft.Extensions.Hosting;
+using Steeltoe.Discovery.Client;
 using Steeltoe.Discovery.Eureka;
 using Steeltoe.Extensions.Configuration.CloudFoundry;
+using System.Threading.Tasks;
 
 namespace Register
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            // Build application configuration
-            ConfigurationBuilder builder = new ConfigurationBuilder();
-            builder.SetBasePath(Environment.CurrentDirectory);
-            builder.AddJsonFile("appsettings.json");
-            builder.AddCloudFoundry();
-            var configuration = builder.Build();
-
-
-            // Setup logging
-            var factory = GetLoggerFactory();
-
-
-            // Build Eureka clients config from configuration
-            var clientOpts = new EurekaClientOptions();
-            ConfigurationBinder.Bind(configuration.GetSection(EurekaClientOptions.EUREKA_CLIENT_CONFIGURATION_PREFIX), clientOpts);
-
-            // Build Eureka instance info config from configuration
-            var instOpts = new EurekaInstanceOptions();
-            ConfigurationBinder.Bind(configuration.GetSection(EurekaInstanceOptions.EUREKA_INSTANCE_CONFIGURATION_PREFIX), instOpts);
-
-            // if a Cloud Foundry Service Binding is found, update config
-            var si = configuration.GetServiceInfos<EurekaServiceInfo>();
-            if (si.Any())
-            {
-                EurekaPostConfigurer.UpdateConfiguration(configuration, si.First(), clientOpts);
-                EurekaPostConfigurer.UpdateConfiguration(configuration, si.First(), instOpts);
-            }
-
-            // Initialize ApplicationManager with instance configuration
-            ApplicationInfoManager.Instance.Initialize(instOpts, factory);
-
-            // Create the Eureka client, the Application is registered and renewed with registry
-            var client = new DiscoveryClient(clientOpts, null, factory);
-
-            // Hang and keep renewing the registration
-            Console.ReadLine();
+            await Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration(config => config.AddCloudFoundry())
+                .AddServiceDiscovery(client => client.UseEureka())
+                .RunConsoleAsync();
         }
-
-
-        public static ILoggerFactory GetLoggerFactory()
-        {
-            IServiceCollection serviceCollection = new ServiceCollection();
-            serviceCollection.AddLogging(builder => builder.SetMinimumLevel(LogLevel.Trace));
-            serviceCollection.AddLogging(builder => builder.AddConsole((opts) =>
-            {
-                opts.DisableColors = true;
-            }));
-            serviceCollection.AddLogging(builder => builder.AddConsole());
-            return serviceCollection.BuildServiceProvider().GetService<ILoggerFactory>();
-        }
-
     }
 }
