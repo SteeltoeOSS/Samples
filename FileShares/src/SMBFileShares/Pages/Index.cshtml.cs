ea4747b0206cd0342b1c203d899f4928471c57f7
Sergei Korolev <korser1@gmail.com>
2022-02-09 11:28:37 -0500

    Update SMBFileShares to .Net core 3.1, 6
    * fix links to scripts
    * use props file

--- FileShares/src/SMBFileShares/Pages/Index.cshtml.cs
+++ FileShares/src/SMBFileShares/Pages/Index.cshtml.cs
@@ -1,11 +1,6 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace SMBFileShares.Controllers
+namespace SMBFileShares.Pages
 {
     public class IndexModel : PageModel
     {
--- FileShares/src/SMBFileShares/Program.cs
+++ FileShares/src/SMBFileShares/Program.cs
@@ -1,5 +1,5 @@
-﻿using Microsoft.AspNetCore;
-using Microsoft.AspNetCore.Hosting;
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
 using Steeltoe.Common.Hosting;
 using Steeltoe.Extensions.Configuration.CloudFoundry;
 
@@ -9,13 +9,16 @@ namespace SMBFileShares
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            CreateHostBuilder(args).Build().Run();
         }
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .ConfigureAppConfiguration(cfg => cfg.AddCloudFoundry())
-                .UseCloudHosting()
-                .UseStartup<Startup>();
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .AddCloudFoundryConfiguration()
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                })
+                .UseCloudHosting();
     }
 }
