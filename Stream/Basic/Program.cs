eee2dabbe84965518e8c7256389f4bb7826df33e
Hananiel Sarella <hananiel@gmail.com>
2022-10-12 22:55:19 -0400

    Update Basic Sample to use WebApplicationBuilder (#252)
    * Update Basic Sample to use WebApplicationBuilder
    
    * Apply suggestions from code review
    
    * PR changes
    
    Co-authored-by: Tim Hess <thess@vmware.com>

--- Stream/Basic/Program.cs
+++ Stream/Basic/Program.cs
@@ -1,35 +1,9 @@
-﻿using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using Steeltoe.Messaging.Handler.Attributes;
-using Steeltoe.Stream.Attributes;
-using Steeltoe.Stream.Messaging;
-using Steeltoe.Stream.StreamHost;
-using System.Threading.Tasks;
+﻿using Basic;
+using Microsoft.AspNetCore.Builder;
+using Steeltoe.Stream.Extensions;
 
-namespace Basic
-{
-    [EnableBinding(typeof(IProcessor))]
-    public class Program
-    {
-        static async Task Main(string[] args)
-        {
-            await StreamHost.CreateDefaultBuilder<Program>(args)
-              .ConfigureServices((context, services) =>
-              {
-                  services.AddLogging(builder =>
-                  {
-                      builder.AddDebug();
-                      builder.AddConsole();
-                  });
-              }).RunConsoleAsync();
-        }
 
-        [StreamListener(IProcessor.INPUT)]
-        [SendTo(IProcessor.OUTPUT)]
-        public string Handle(string inputVal)
-        {
-            return inputVal.ToUpper();
-        }
-    }
-}
+var builder = WebApplication.CreateBuilder(args);
+builder.Host.AddStreamServices<MyStreamProcessor>();
+
+builder.Build().Run();
