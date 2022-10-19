2034c803fe34f50f28bd88f4dfad78216a0760ba
Tim Hess <thess@vmware.com>
2022-10-19 17:05:02 -0500

    add error handling for when Lua scripting is disabled

--- Connectors/src/Redis/Controllers/HomeController.cs
+++ Connectors/src/Redis/Controllers/HomeController.cs
@@ -26,8 +26,8 @@ namespace Redis.Controllers
 
         public IActionResult CacheData()
         {
-            string key1 = Encoding.UTF8.GetString(_cache.Get("Key1"));
-            string key2 = Encoding.UTF8.GetString(_cache.Get("Key2"));
+            var key1 = Encoding.UTF8.GetString(_cache.Get("Key1"));
+            var key2 = Encoding.UTF8.GetString(_cache.Get("Key2"));
 
             ViewData["Key1"] = key1;
             ViewData["Key2"] = key2;
@@ -37,13 +37,24 @@ namespace Redis.Controllers
 
         public IActionResult ConnData()
         {
-            IDatabase db = _conn.GetDatabase();
+            var db = _conn.GetDatabase();
 
             string key1 = db.StringGet("ConnectionMultiplexerKey1");
             string key2 = db.StringGet("ConnectionMultiplexerKey2");
 
             ViewData["ConnectionMultiplexerKey1"] = key1;
             ViewData["ConnectionMultiplexerKey2"] = key2;
+
+            try
+            {
+                var prepared = LuaScript.Prepare("local val=\"Hello from Lua\" return val");
+                var result = db.ScriptEvaluate(prepared);
+                ViewData["LuaResponse"] = result.ToString();
+            }
+            catch
+            {
+                ViewData["LuaResponse"] = "Failed to execute Lua script, scripting is likely not enabled";
+            }
 
             return View();
         }
