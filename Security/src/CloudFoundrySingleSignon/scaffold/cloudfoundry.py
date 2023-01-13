e6b8e98fcc2d2bf632811665c86ae5a65311c759
Chris Cheetham <chris@cheetham.com>
2023-01-13 13:54:50 -0500

    wip

--- Security/src/CloudFoundrySingleSignon/scaffold/cloudfoundry.py
+++ Security/src/CloudFoundrySingleSignon/scaffold/cloudfoundry.py
@@ -10,20 +10,20 @@ def setup(context):
     """
     :type context: behave.runner.Context
     """
+    cf = cloudfoundry.CloudFoundry(context)
     uaa = 'uaa'
     sso = 'single-signon'
-    cf = cloudfoundry.CloudFoundry(context)
+    domain = 'apps.{}'.format(context.options.cf.apiurl.removeprefix('https://api.sys.'))
+    uaa_url = 'uaa://{}.{}'.format(uaa, domain)
+    sso_url = 'https://{}.{}'.format(sso, domain)
     # remove previous apps
     cf.delete_app(sso)
     cf.delete_app(uaa)
     # create UAA service and app
     if not cf.service_exists('myOAuthService'):
-        credentials = '\'{{"client_id":"myTestApp", "client_secret":"myTestApp", "uri":"{}"}}\''.format(
-            dns.resolve_url(context, 'uaa://uaa'))
+        credentials = '\'{{"client_id":"myTestApp", "client_secret":"myTestApp", "uri":"{}"}}\''.format(uaa_url)
         cf.create_user_provided_service('myOAuthService', credentials)
     if not cf.app_exists(uaa):
-        fqdn = dns.resolve_hostname(context, uaa)
-        domain = fqdn.removeprefix("{}.".format(uaa))
         uaa_repo = os.path.join(context.project_dir, uaa)
         if os.path.exists(uaa_repo):
             def remove_readonly(func, path, excinfo):
@@ -39,13 +39,13 @@ def setup(context):
             Command(context, cmd_s).run()
         cf.push_app('uaa/build/sample-manifests/uaa-cf-application.yml')
         for cmd_s in [
-            'uaac target {}'.format(dns.resolve_url(context, 'https://uaa')),
+            'uaac target {}'.format(uaa_url),
             'uaac token client get admin -s adminsecret',
             'uaac contexts',
             'uaac group add testgroup',
             'uaac user add testuser --given_name Test --family_name User --emails testuser@domain.com --password Password1!',
             'uaac member add testgroup testuser',
             'uaac client add myTestApp --scope cloud_controller.read,cloud_controller_service_permissions.read,openid,testgroup --authorized_grant_types authorization_code,refresh_token --authorities uaa.resource --redirect_uri {} --autoapprove cloud_controller.read,cloud_controller_service_permissions.read,openid,testgroup --secret myTestApp'.format(
-                'https://{}/signin-cloudfoundry'.format(sso)),
+                '{}/signin-cloudfoundry'.format(sso_url))
         ]:
             Command(context, cmd_s).run()
