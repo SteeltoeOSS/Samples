06bd79ea8ad62fd0d52432b93bc1ef1828044605
Bart Koelman <104792814+bart-vmware@users.noreply.github.com>
2023-04-03 14:59:23 +0200

    Update CosmosDB connector sample using new CloudFoundry service bindings

--- Connectors/src/CosmosDb/Controllers/HomeController.cs
+++ Connectors/src/CosmosDb/Controllers/HomeController.cs
@@ -1,32 +1,64 @@
 ﻿using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using System.Text.Json;
 using CosmosDb.Data;
 using CosmosDb.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Cosmos;
+using Microsoft.Azure.Cosmos.Linq;
+using Steeltoe.Connector;
+using Steeltoe.Connector.CosmosDb;
 
 namespace CosmosDb.Controllers;
 
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
-    private readonly ICosmosDbService _cosmosDbService;
+    private readonly ConnectionProvider<CosmosDbOptions, CosmosClient> _connectionProvider;
 
-    public HomeController(ILogger<HomeController> logger, ICosmosDbService cosmosDbService)
+    public HomeController(ILogger<HomeController> logger, ConnectionFactory<CosmosDbOptions, CosmosClient> connectionFactory)
     {
         _logger = logger;
-        _cosmosDbService = cosmosDbService;
+        _connectionProvider = connectionFactory.GetDefault();
     }
 
     public async Task<IActionResult> Index(CancellationToken cancellationToken)
     {
-        // Steeltoe: Fetch objects from CosmosDB container.
-        var model = new CosmosDbViewModel();
+        // Steeltoe: Fetch objects from CosmosDB container. Do not dispose the CosmosClient singleton.
+        CosmosClient client = _connectionProvider.CreateConnection();
+        Container container = client.GetContainer(_connectionProvider.Options.Database, CosmosDbSeeder.ContainerId);
 
-        await foreach (SampleObject sampleObject in _cosmosDbService.GetAllAsync(cancellationToken))
+        var model = new CosmosDbViewModel
+        {
+            ConnectionString = _connectionProvider.Options.ConnectionString,
+            OptionsJson = JsonSerializer.Serialize(client.ClientOptions, new JsonSerializerOptions
+            {
+                WriteIndented = true
+            }),
+            Database = _connectionProvider.Options.Database
+        };
+
+        await foreach (SampleObject sampleObject in GetAllAsync(container, cancellationToken))
         {
             model.SampleObjects.Add(sampleObject);
         }
 
         return View(model);
+    }
+
+    private async IAsyncEnumerable<SampleObject> GetAllAsync(Container container, [EnumeratorCancellation] CancellationToken cancellationToken)
+    {
+        using FeedIterator<SampleObject> iterator = container.GetItemLinqQueryable<SampleObject>().ToFeedIterator();
+
+        while (iterator.HasMoreResults)
+        {
+            FeedResponse<SampleObject> response = await iterator.ReadNextAsync(cancellationToken);
+
+            foreach (SampleObject sampleObject in response)
+            {
+                yield return sampleObject;
+            }
+        }
     }
 
     public IActionResult Privacy()
--- Connectors/src/CosmosDb/Models/CosmosDbViewModel.cs
+++ Connectors/src/CosmosDb/Models/CosmosDbViewModel.cs
@@ -4,5 +4,9 @@ namespace CosmosDb.Models;
 
 public sealed class CosmosDbViewModel
 {
+    public string? ConnectionString { get; set; }
+    public string? OptionsJson { get; set; }
+    public string? Database { get; set; }
+
     public List<SampleObject> SampleObjects { get; set; } = new();
 }
