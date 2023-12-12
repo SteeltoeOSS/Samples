c350fd29413d4e29855b4f030d17876462c93a03
Bart Koelman <104792814+bart-vmware@users.noreply.github.com>
2023-12-12 15:16:05 +0100

    Update Redis session state sample to use new connector

--- Security/src/RedisDataProtectionKeyStore/Program.cs
+++ Security/src/RedisDataProtectionKeyStore/Program.cs
@@ -1,22 +1,41 @@
-﻿using Microsoft.AspNetCore;
-using Microsoft.AspNetCore.Hosting;
-using Steeltoe.Common.Hosting;
-using Steeltoe.Extensions.Configuration.CloudFoundry;
+using Microsoft.AspNetCore.DataProtection;
+using Steeltoe.Connectors.Redis;
+using Steeltoe.Security.DataProtection.Redis;
 
-namespace RedisDataProtectionKeyStore
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+// Steeltoe: Setup Redis connector.
+builder.AddRedis();
+
+// Steeltoe: Setup data protection to use Redis connector.
+builder.Services.AddDataProtection().PersistKeysToRedis().SetApplicationName("redis-keystore");
+
+// Steeltoe: Enable session state.
+builder.Services.AddSession();
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+WebApplication app = builder.Build();
+
+// Steeltoe: Activate session state.
+app.UseSession();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            BuildWebHost(args).Run();
-        }
-
-        public static IWebHost BuildWebHost(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                    .UseCloudHosting()
-                    .AddCloudFoundryConfiguration()
-                    .UseStartup<Startup>()
-                    .Build();
-    }
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
 }
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
