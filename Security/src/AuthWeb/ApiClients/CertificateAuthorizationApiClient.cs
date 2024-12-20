f7f50342f4efcb9dc829f0af112705eee111d61a
Bart Koelman <104792814+bart-vmware@users.noreply.github.com>
2024-12-20 16:49:45 +0100

    Update auth sample to inject typed HttpClient, improve logging and error display (#358)

---
+++ Security/src/AuthWeb/ApiClients/CertificateAuthorizationApiClient.cs
@@ -0,0 +1,20 @@
+﻿using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using Steeltoe.Samples.AuthWeb.Models;
+
+namespace Steeltoe.Samples.AuthWeb.ApiClients;
+
+public sealed class CertificateAuthorizationApiClient(HttpClient httpClient)
+    : StringApiClient(httpClient)
+{
+    public async Task<AuthApiResponseModel> GetSameOrgAsync(CancellationToken cancellationToken)
+    {
+        return await GetAsync("/api/certificate/SameOrg", cancellationToken);
+    }
+
+    public async Task<AuthApiResponseModel> GetSameSpaceAsync(CancellationToken cancellationToken)
+    {
+        return await GetAsync("/api/certificate/SameSpace", cancellationToken);
+    }
+}
---
+++ Security/src/AuthWeb/ApiClients/JwtAuthorizationApiClient.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading;
+using System.Threading.Tasks;
+using Steeltoe.Samples.AuthWeb.Models;
+
+namespace Steeltoe.Samples.AuthWeb.ApiClients;
+
+public sealed class JwtAuthorizationApiClient(HttpClient httpClient)
+    : StringApiClient(httpClient)
+{
+    public async Task<AuthApiResponseModel> GetAuthorizationAsync(string? accessToken, CancellationToken cancellationToken)
+    {
+        if (string.IsNullOrEmpty(accessToken))
+        {
+            return new AuthApiResponseModel
+            {
+                Error = new InvalidOperationException(
+                    "No access token found in user session. Perhaps you need to set 'Authentication:Schemes:OpenIdConnect:SaveTokens' to 'true'?")
+            };
+        }
+
+        HttpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
+        return await GetAsync("/api/JwtAuthorization", cancellationToken);
+    }
+}
