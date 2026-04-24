69d12d9f7d844b3f218d0e477f2d1f634d88fa51
Bart Koelman <104792814+bart-vmware@users.noreply.github.com>
2026-04-24 13:40:50 +0200

    Convert to .slnx format

--- tools/sync-DotSettings/sync-DotSettings.ps1
+++ tools/sync-DotSettings/sync-DotSettings.ps1
@@ -1,6 +1,6 @@
 #Requires -Version 7.4
 
-# This script copies the Resharper/Rider code style settings (.sln.DotSettings file) from Steeltoe, 
+# This script copies the Resharper/Rider code style settings (.slnx.DotSettings file) from Steeltoe, 
 # patches it for use in samples, then distributes it across the samples. It enables IDE IntelliSense and code cleanup.
 # The patch file was created with: `git diff --unified=0 > DotSettings.patch`
 
@@ -8,10 +8,10 @@
 $ErrorActionPreference = "Stop"
 $PSNativeCommandUseErrorActionPreference = $true
 
-Copy-Item "../../../Steeltoe/src/Steeltoe.All.sln.DotSettings" -Destination "baseline.DotSettings"
+Copy-Item "../../../Steeltoe/src/Steeltoe.All.slnx.DotSettings" -Destination "baseline.DotSettings"
 git apply --unidiff-zero --recount DotSettings.patch
 
-Get-ChildItem "../../" -Recurse -Filter "Steeltoe.Samples.*.sln" | ForEach-Object {
+Get-ChildItem "../../" -Recurse -Filter "Steeltoe.Samples.*.slnx" | ForEach-Object {
   $targetFileName = "$($_.Name).DotSettings"
   $targetPath = [System.IO.Path]::Combine([System.IO.Path]::GetDirectoryName($_.FullName), $targetFileName)
   Copy-Item "baseline.DotSettings" -Destination $targetPath
