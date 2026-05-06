6d9968027aa174dbacdde51798d6e0154988136f
Bart Koelman <104792814+bart-vmware@users.noreply.github.com>
2026-05-06 14:28:18 +0200

    Update Integration/Messaging/Stream projects to .NET 8

--- configure.ps1
+++ configure.ps1
@@ -13,18 +13,6 @@ $repoRoot = "$PSScriptRoot"
 $global:seenProjectFiles = @()
 $global:seenPropsFiles = @()
 
-function syncNet60() {
-    $sourceFilePath = Resolve-Path -Path "$repoRoot/config/Directory.Build.net60.props"
-    foreach ($projectFilePath in Get-ChildItem -Path $repoRoot -Recurse -Filter '*.csproj' | where { $_ | Select-String -SimpleMatch '<TargetFramework>net6.0</TargetFramework>' }) {
-        $targetDir = Split-Path -Parent $projectFilePath
-        $targetFilePath = Join-Path -Path $targetDir -ChildPath 'Directory.Build.props'
-        Copy-Item $sourceFilePath -Destination $targetFilePath -Force
-
-        $global:seenProjectFiles += $projectFilePath.FullName
-        $global:seenPropsFiles += $targetFilePath
-    }
-}
-
 function syncNet80() {
     $sourceFilePath = Resolve-Path -Path "$repoRoot/config/Directory.Build.net80.props"
     foreach ($projectFilePath in Get-ChildItem -Path $repoRoot -Recurse -Filter '*.csproj' | where { $_ | Select-String -SimpleMatch '<TargetFramework>net8.0</TargetFramework>' }) {
@@ -50,7 +38,5 @@ function verify() {
     }
 }
 
-syncNet60
 syncNet80
 verify
-
